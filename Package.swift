60646d838afbb9ec00a580ecc80eff36c3033633
Alon Genosar <alon@bootleg.co.il>
2020-06-14 09:41:34 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
 
+let package = Package(
+    name: "Drop",
+    platforms: [
+        .iOS
+    ],
+    products: [
+        .library(name: "Drop", targets: ["Drop"]),
+    ],
+    targets: [
+        .target(name: "Drop")
+    ]
+)
